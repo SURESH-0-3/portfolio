db1c686627b08d79a767539a9e7c748fced1db17
KaMaLeSh-junior <starkkamalesh@gmail.com>
2024-08-19 22:17:41 +0530

    towards success

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -726,9 +726,7 @@ $(function () {
 const form = document.querySelector("#myForm");
 async function handleSubmit(e) {
   e.preventDefault();
-  console.log('here');
   const FORMPREE= 'https://formspree.io/f/manwdjgw';
-
   const status = document.querySelector("#my-form-status");
   const data = new FormData(e.target);
   fetch(FORMPREE, {
@@ -738,13 +736,10 @@ async function handleSubmit(e) {
         'Accept': 'application/json'
     }
   }).then(response => {
-    console.log("sent successfully");
-    
     form.reset();
       status.type='text';
     status.value = "Thanks for your submission!";
   }).catch(error => {
-    console.log("sent un-successfully");
     status.type='text';
     status.value = "Oops! There was a problem submitting your form"
   });
